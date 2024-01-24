251c794b1786f6f3b8a4682f2917e7e50d4f2164
Carl Molina <carlandrew.molina@gmail.com>
2024-01-24 14:34:49 -0800

    refactored docstring, removed old ref code, added console.log

--- jobly/src/JobCardList.js
+++ jobly/src/JobCardList.js
@@ -3,34 +3,28 @@ import JobCard from "./JobCard";
 /** JobCardList: Renders a list of job cards from filtered jobs.
  *
  * Props:
- * - filteredJobs from searchbar:
- *    [{ id, salary, equity, title, companyHandle, companyName }, ...]
+ *   One of the following:
+ *    - filteredJobs from JobList:
+ *      [{ id, salary, equity, title, companyHandle, companyName }, ...]
  *
- * - filteredJobs from clicking on company detail:
- *    [{ id, salary, equity, title}, ...]
- *
+ *    - filteredJobs from CompanyDetail:
+ *      [{ id, salary, equity, title}, ...]
  *
  * State:
- * none
+ * - none
  *
  * JobList -> JobCardList -> JobCard
 */
 
-// FIXME: update props for this docstring^^^^^
-
 function JobCardList({ filteredJobs }) {
+
+  console.log('JobCardList render: ', filteredJobs);
 
   return (
     <div>
       {filteredJobs.map(j => (
         <JobCard key={j.id} job={j} />
       ))}
-
-      {/* old ref: */}
-      {/* <p>JOB CARD LIST</p>
-      <JobCard />
-      <JobCard />
-      <JobCard /> */}
     </div>
   )
 }
