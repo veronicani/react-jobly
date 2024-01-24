993281a7163c8098b513f1a182a22698f2568f69
Veronica Ni <vni1294@gmail.com>
2024-01-24 17:07:33 -0500

    Implemented CompanyCard rendering

--- jobly/src/CompanyCard.js
+++ jobly/src/CompanyCard.js
@@ -12,12 +12,19 @@ import { Link } from "react-router-dom";
 
 function CompanyCard({ company }) {
   // console.log("CompanyCard input: ", company);
+  const { handle, name, description, numEmployees, logoUrl } = company;
+
 
   return (
     <div className="CompanyCard">
-      {/* <Link to={`/companies/${company.handle}`} > */}
-        <p>COMPANY CARD</p>
-      {/* </Link> */}
+      <Link to={`/companies/${handle}`} >
+        <div className="CompanyCard-header">
+          <h3>{name}</h3>
+          {logoUrl && 
+          <img src={`../${logoUrl}`} alt={`${name} logo`} width="50px"/>}
+        </div>
+        <p>{description}</p>
+      </Link>
     </div>
   )
 }
