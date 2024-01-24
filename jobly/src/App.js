3817a970d47d88e514eeb9eb26fae014cd902d4c
Veronica Ni <vni1294@gmail.com>
2024-01-23 20:54:15 -0500

    Added browser router and navbar

--- jobly/src/App.js
+++ jobly/src/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
-
+import Navbar from "./Navbar";
+import RoutesList from "./RoutesList";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <RoutesList />
+      </BrowserRouter>
     </div>
   );
 }
