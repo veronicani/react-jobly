d34fca3e92facf9e82357a11580ff6b9c5d50359
Carl Molina <carlandrew.molina@gmail.com>
2024-01-24 14:35:05 -0800

    created first draft of JobList component

--- jobly/src/JobList.js
+++ jobly/src/JobList.js
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import JobCardList from "./JobCardList";
 import SearchBar from "./SearchBar";
+
+import JoblyApi from "./api";
 
 /** JobList: Renders a list of jobs and the search bar. Makes API request to
  * get all job data based on search terms.
@@ -17,16 +19,56 @@ import SearchBar from "./SearchBar";
 */
 
 function JobList() {
-  //TODO: useEffect!
-  
+  const [searchTerm, setSearchTerm] = useState("");
+  const [jobList, setJobList] = useState({
+    data: null,
+    isLoading: true,
+    errors: null,
+  });
+
+  console.log('JobList jobList state: ', jobList);
+  console.log('JobList searchTerm state: ', searchTerm);
+
+  useEffect(function fetchFilteredJobsOnSearch() {
+    async function fetchJobs() {
+      try {
+        const jobsData = await JoblyApi.getJobs(searchTerm);
+        setJobList({
+          data: jobsData,
+          isLoading: false
+        });
+      } catch (err) {
+        setJobList({
+          data: null,
+          isLoading: false,
+          errors: err,
+        });
+      }
+    }
+    fetchJobs();
+  }, [searchTerm]);
+
   function search(job) {
     console.log("JobList search");
+    setJobList({data: null, isLoading: true, errors: null});
+    setSearchTerm(job);
   }
-  
+
+  if (jobList.isLoading) return <i>Loading...</i>
+  else if (jobList.errors) return <i>Server error. Please try again.</i>
+  else if (jobList.data.length === 0) return (
+    <div className="JobList">
+      <SearchBar search={search} />
+      <i>No jobs found for '{searchTerm}'</i>
+    </div>
+  )
+
+  console.log('jobList: ', jobList);
+
   return (
     <div>
       <SearchBar search={search} />
-      <JobCardList />
+      <JobCardList filteredJobs={jobList.data} />
     </div>
   )
 }
