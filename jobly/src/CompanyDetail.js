e1133cf513610b391cc73a5059874379885c5131
Veronica Ni <vni1294@gmail.com>
2024-01-24 14:05:39 -0500

    Implemented API call for company details

--- jobly/src/CompanyDetail.js
+++ jobly/src/CompanyDetail.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { useParams, Navigate } from "react-router-dom";
+import JoblyApi from "./api";
 
 /** CompanyDetail: logical component for single company. Makes a request to
  * Jobly API for data about a company.
@@ -6,7 +8,7 @@ import { useParams, Navigate } from "react-router-dom";
  *
  *  Props: none
  *
- *  State: none
+ *  State: TODO: update state to be companyDetail
  *
  *  RoutesList -> CompanyDetail -> JobCardList
  */
@@ -14,13 +16,38 @@ import { useParams, Navigate } from "react-router-dom";
 function CompanyDetail() {
   const { handle } = useParams();
   console.log('CompanyDetail handle:', handle);
+  const [companyDetail, setCompanyDetail] = useState({
+    data: null,
+    isLoading: true,
+    errors: null
+  });
+  console.log("CompanyDetail state: ", companyDetail);
 
+  useEffect(function getCompanyDataOnMount() {
+    async function fetchCompanyData() {
+      try {
+        const companyData = await JoblyApi.getCompany(handle);
+        setCompanyDetail({
+          data: companyData,
+          isLoading: false
+        });
+      } catch (err) {
+        setCompanyDetail({
+          data: null,
+          isLoading: false,
+          errors: err
+        });
+      }
+    }
+    fetchCompanyData();
+  }, []);
 
-
-  if (!handle) return <Navigate to="/companies" />
+  if (companyDetail.isLoading) return <i>Loading...</i>
+  else if (companyDetail.errors) return <Navigate to="/companies" />
 
   return (
     <div className="CompanyDetail">
+      {/* TODO: company name, description, and list of jobs from the company. */}
       <h3>This is company detail.</h3>
     </div>
   );
