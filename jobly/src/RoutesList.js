96d46c5cf5f03376ef7097fbdb7624a50df08226
Veronica Ni <vni1294@gmail.com>
2024-01-25 18:59:58 -0500

    Added routes for signup and profile

--- jobly/src/RoutesList.js
+++ jobly/src/RoutesList.js
@@ -1,9 +1,13 @@
 import { Navigate, Routes, Route } from "react-router-dom";
 
+import SignUpForm from "./SignUpForm";
+import LoginForm from "./LoginForm";
+import ProfileForm from "./ProfileForm";
+
+import Homepage from "./Homepage";
+import JobList from "./JobList";
 import CompanyList from "./CompanyList";
 import CompanyDetail from "./CompanyDetail";
-import JobList from "./JobList";
-import Homepage from "./Homepage";
 
 
 /** RoutesList: list of routes for Jobly app.
@@ -24,6 +28,8 @@ function RoutesList() {
       <Route path="/jobs" element={<JobList />} />
 
       <Route path="/login" element={<LoginForm />} />
+      <Route path="/signup" element={<SignUpForm />} />
+      <Route path="/profile" element={<ProfileForm />} />
 
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
