f96048d86ca8f0917139f668aef87cdb42a5bdca
Veronica Ni <vni1294@gmail.com>
2024-01-25 18:58:56 -0500

    Stubbed out navbar links

--- jobly/src/Navbar.js
+++ jobly/src/Navbar.js
@@ -16,9 +16,12 @@ function Navbar() {
       <nav>
         <NavLink to="/" className="Navbar-title">Jobly</NavLink>
         <ul>
+        <li><NavLink to="/login">ANON Login</NavLink></li>
+          <li><NavLink to="/signup">ANON Signup</NavLink></li>
           <li><NavLink to="/companies">Companies</NavLink></li>
-          <li><NavLink to="/companies/baker-santos">Baker Santos</NavLink></li>
           <li><NavLink to="/jobs">Jobs</NavLink></li>
+          <li><NavLink to="/profile">USER Profile</NavLink></li>
+          <li>USER LOGOUT BTN</li>
         </ul>
       </nav>
     </div>
