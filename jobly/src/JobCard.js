5de2cc9326472f0bddb72d42f86b2a43bc5c5ed2
Carl Molina <carlandrew.molina@gmail.com>
2024-01-25 11:38:31 -0800

    refactored docstring for job props

--- jobly/src/JobCard.js
+++ jobly/src/JobCard.js
@@ -4,25 +4,21 @@ import "./JobCard.css";
  *  salary, and equity.
  *
  *  Props:
- *  job - one of the following:
- *  //TODO: list what it NEEDS, to render (companyName is optional)
- *   - from JobCardList: {id, salary, equity, title, companyHandle, companyName}
- *   - from CompanyDetail: {id, salary, equity, title}
+ *  job -> { title, salary, equity, companyName (optional) }
  *
  *  State: none
  *
  *  JobCardList -> JobCard
  */
-//TODO: some jobs don't have salary/equity, use &&
+
 function JobCard({ job }) {
   console.log("JobCard input: ", job);
-  //TODO: destructure job
 
   const { title, companyName, salary, equity } = job;
 
   return (
     <div className="JobCard">
-      <h3 className="JobCard-title">{job.title}</h3>
+      <h3 className="JobCard-title">{title}</h3>
       {companyName &&
         <p>Company: {companyName}</p>}
       {salary &&  <p>Salary: {salary}</p>}
