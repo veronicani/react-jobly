2671e1e2b68f176bd4bc43020452289be982a219
Carl Molina <carlandrew.molina@gmail.com>
2024-01-25 16:23:07 -0800

    added first draft signUp fn

--- jobly/src/App.js
+++ jobly/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./Navbar";
 import RoutesList from "./RoutesList";
 
 import LoginForm from "./LoginForm";
+import JoblyApi from "./api";
 
 /** Jobly App.
  *
@@ -15,9 +16,23 @@ import LoginForm from "./LoginForm";
  * App -> { Navbar, RoutesList }
  */
 function App() {
+  const [userData, setUserData] = useState({
+    token: "",
 
+  })
 
-  function signUp() {
+  async function signUp(formData) {
+    const { username, password, firstName, lastName, email } = formData
+    const receivedToken = await JoblyApi
+      .registerUser(username, password, firstName, lastName, email);
+
+    setUserData(uData => ({
+      username: username,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      token: receivedToken,
+    }));
 
   }
 
@@ -29,7 +44,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <RoutesList />
+        <RoutesList handleSignUp={signUp}/>
       </BrowserRouter>
     </div>
   );
