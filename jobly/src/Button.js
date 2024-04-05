ff8ac3e1b147f9154aeeadf97beedd6ec98e7d4c
Veronica Ni <vni1294@gmail.com>
2024-04-05 17:41:33 -0400

    Added homepage + button tweaks
    - Removed -1 index to allow buttons to be clicked
    - added clickHandler to allow routing to links
    - fixed Homepage dimensions to take up full space of App
    - Added signup/login buttons if anon user, jobs buttons if signed in

--- jobly/src/Button.js
+++ jobly/src/Button.js
@@ -5,17 +5,20 @@ import "./Button.scss";
  * Props:
  * - classes: additional classes outside of Bootstrap.
  * - label: text to display on the button.
+ * - handleClick: parent function to call when button is clicked.
  * 
  * State:
  * none.
  * 
  * { SearchBar } -> Button
 */
-function Button({ classes, label }) {
+function Button({ classes, label, handleClick }) {
   return (
     <button 
-      className={`${classes} btn btn-secondary`}>
-        {label}
+      className={`${classes} btn btn-secondary`}
+      onClick={handleClick}
+    >
+      {label}
     </button>
   )
 }
--- jobly/src/Homepage.js
+++ jobly/src/Homepage.js
@@ -1,8 +1,10 @@
 import "./Homepage.scss";
 import hero from "./images/hero.jpeg"
 
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import userContext from "./userContext";
+import Button from "./Button";
 
 /** Homepage: component for landing page on Jobly app.
  *
@@ -16,21 +18,44 @@ import userContext from "./userContext";
 function Homepage() {
 
   const { user } = useContext(userContext);
-
   const { username } = user;
 
+  const navigate = useNavigate();
 
   return (
-    <div className="Homepage row">
-      <div className="col-6 mx-auto my-auto">
+    <div className="Homepage row m-0">
+      <div className="col-12 col-sm-8 col-md-6 mx-auto my-auto">
       <h1>Jobly</h1>
       <h2>All the jobs in one convenient place.</h2>
       <img 
-        className="Homepage-hero-img img-fluid my-2"
+        className="Homepage-hero-img img-fluid my-3"
         src={hero} 
         alt="Hero illustration of office workers"
       />
-      {username && <h4>Welcome, {username}! GET THOSE JOBS!!</h4>}
+      {username &&
+        <>
+        <h4>Welcome, {username}! GET THOSE JOBS!!</h4>
+        <Button 
+          classes="Homepage-btn my-2"
+          label="Browse listings" 
+          handleClick={() => navigate("/jobs")}
+        />
+        </>
+      }
+      {!username &&
+        <div className="d-flex justify-content-center gap-4">
+         <Button 
+          classes="Homepage-btn"
+          label="Sign Up" 
+          handleClick={() => navigate("/signup")}
+        />
+         <Button 
+          classes="Homepage-btn"
+          label="Login" 
+          handleClick={() => navigate("/login")}
+        />
+        </div>
+      }
       </div>
       
     </div>
