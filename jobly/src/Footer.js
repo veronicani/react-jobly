a05029ace7772152d3735bbc295529b44919937f
Veronica Ni <vni1294@gmail.com>
2024-04-05 19:51:59 -0400

    Added additional credit

--- jobly/src/Footer.js
+++ jobly/src/Footer.js
@@ -17,7 +17,10 @@ function Footer() {
       <div className="container">
         <div className="row">
           <div className="col d-flex justify-content-between align-items-center py-4">
-            <small className="">Background image from Vecteezy.</small>
+            <div className="row p-0">
+              <small className="text-start">Hero image from Adobe Stock.</small>
+              <small className="text-start">Background image from Vecteezy.</small>
+            </div>
             {/* <p className="mb-0">&#169; Carl Molina & Veronica Ni 2024.</p> */}
             <a href="https://github.com/veronicani/react-jobly">
               <i className="bi bi-github"></i>
