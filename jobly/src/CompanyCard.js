f38abb31896ad83fd1a250e464f47b32292db923
Veronica Ni <vni1294@gmail.com>
2024-04-05 20:17:40 -0400

    Migrated to sass

--- jobly/src/CompanyCard.js
+++ jobly/src/CompanyCard.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./CompanyCard.css";
+import "./CompanyCard.scss";
 
 /** CompanyCard: presentational component, shows name of company, company logo,
  *  and description.
