225c5303bb10b394222ce751e17c24febcd69b1d
Veronica Ni <vni1294@gmail.com>
2024-04-26 20:17:12 -0400

    Refactored: updated form el placement

--- jobly/src/LoginForm.js
+++ jobly/src/LoginForm.js
@@ -58,62 +58,63 @@ function LoginForm({ login }) {
 
   return (
     <div className="LoginForm-wrapper row">
-      <div className="LoginForm-container col-xs-12 col-sm-8 col-md-6 mx-auto my-auto">
+      <form 
+        className="LoginForm col-xs-12 col-sm-8 col-md-6 mx-auto my-auto"
+        onSubmit={handleSubmit}
+      >
         <div className="card p-2">
           <h3>Login</h3>
           <small>(username: guest | password: password)</small>
           <div className="card-body">
-            <form className="LoginForm" onSubmit={handleSubmit}>
-              <div className="LoginForm-username mb-3">
-                <label 
-                  className="form-label float-start"
-                  htmlFor="LoginForm-input-username"
-                >
-                  Username
-                </label>
-                <input
-                  className="form-control"
-                  id="LoginForm-input-username"
-                  name="username"
-                  placeholder="Username"
-                  onChange={handleChange}
-                  value={username}
-                  aria-label="Username"
-                  required
-                />
-              </div>
-              <div className="LoginForm-password mb-3">
-                <label 
-                  className="form-label float-start"
-                  htmlFor="LoginForm-input-password"
-                >
-                  Password
-                </label>
-                <input
-                  className="form-control"
-                  type="password"
-                  id="LoginForm-input-password"
-                  name="password"
-                  placeholder="Password"
-                  onChange={handleChange}
-                  value={password}
-                  aria-label="Password"
-                  required
-                />
-              </div>
+            <div className="LoginForm-username mb-3">
+              <label 
+                className="form-label float-start"
+                htmlFor="LoginForm-input-username"
+              >
+                Username
+              </label>
+              <input
+                className="form-control"
+                id="LoginForm-input-username"
+                name="username"
+                placeholder="Username"
+                onChange={handleChange}
+                value={username}
+                aria-label="Username"
+                required
+              />
+            </div>
+            <div className="LoginForm-password mb-3">
+              <label 
+                className="form-label float-start"
+                htmlFor="LoginForm-input-password"
+              >
+                Password
+              </label>
+              <input
+                className="form-control"
+                type="password"
+                id="LoginForm-input-password"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                value={password}
+                aria-label="Password"
+                required
+              />
+            </div>
 
-              {errs.map((err, i) => <Alert key={i} message={err} />)}
-    
-              <div className="d-grid">
-                <Button classes="LoginForm-login-btn" label="Submit" />
-              </div>
-              <div className="mt-1">
-                <small>💤 Server may take up to a minute to wake up!</small>
-              </div>
-            </form>
+            {errs.map((err, i) => <Alert key={i} message={err} />)}
+  
+            <div className="d-grid">
+              <Button classes="LoginForm-login-btn" label="Submit" />
+            </div>
+            <div className="mt-1">
+              <small>💤 Server may take up to a minute to wake up!</small>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
